3500d5886f6e11660ab57c211cf5ab361bd3d108
ananazario <anacarolinanazario98@gmail.com>
2023-03-24 11:26:01 -0300

    Fazendo a funcao do ordenador

--- src/pages/Itens/Itens.tsx
+++ src/pages/Itens/Itens.tsx
@@ -12,7 +12,7 @@ interface Props {
 const Itens = (props: Props) => {
 
   const [lista, setLista] = useState(cardapio)
-  const { busca, filtro } = props
+  const { busca, filtro, ordenador } = props
 
   function testaBusca(title : string) {
     const regex = new RegExp(busca, 'i')
@@ -22,13 +22,30 @@ const Itens = (props: Props) => {
   function testaFiltro(id: number) {
     if (filtro !== null) 
       return filtro === id
-    return true;
+    return true
+  }
+
+  function ordenar(novaLista: typeof cardapio) {
+    switch(ordenador) {
+
+      case 'porcao':
+        return novaLista.sort((a, b) => a.tamanho > b.tamanho ? 1 : -1)
+
+      case 'qtd_pessoas': 
+        return novaLista.sort((a, b) => a.servindo > b.servindo ? 1 : -1)
+
+      case 'preco': 
+        return novaLista.sort((a, b) => a.preco > b.preco ? 1 : -1)
+
+      default:
+        return novaLista
+    }
   }
 
   useEffect(() => {
     const novaLista = cardapio.filter((item) => testaBusca(item.titulo) && testaFiltro(item.categoria.id))
-    setLista(novaLista)
-  }, [busca, filtro])
+    setLista(ordenar(novaLista))
+  }, [busca, filtro, ordenador])
 
   return (
     <div className={styles.itens}>
